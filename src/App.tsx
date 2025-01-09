73790a36abb28c1830ee03c0a68b58986dc760dc
Indah Mutiah Utami MZ <indahmutiahmz@gmail.com>
2025-01-09 19:43:16 +0700

    add tailwind and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <header className="bg-yellow-900 text-white p-4">
+        <a href="/" className="flex items-center text-xl font-bold">
+          <img
+            src="/images/logo.png"
+            alt="logo"
+            className="mr-4 rounded-md"
+            width="50"
+            height="50"
+          />
+          Memoman
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </header>
+      <main className="flex flex-col md:flex-row h-screen">
+        <div className="w-full md:w-1/3 bg-gray-50 p-6 overflow-y-auto">
+          <div className="flex flex-col gap-4"></div>
+        </div>
+        <div className="w-full md:w-2/3 bg-white shadow-lg overflow-y-auto">
+          <h1 className="text-2xl font-bold">
+            Welcome to Memoman Task Manager
+          </h1>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

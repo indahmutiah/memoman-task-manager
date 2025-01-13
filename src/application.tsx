f81f81ca57b350e247834b323b2c001d0b29d598
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-13 21:31:10 +0700

    feat: render with component list

--- src/application.tsx
+++ src/application.tsx
@@ -1,6 +1,14 @@
 import { Header } from "./components/header";
 import { TaskList } from "./components/task-list";
+import { Task } from "./types/task";
+
 export function App() {
+  const taskItems: Task[] = [
+    { id: 1, text: "Fixing Bugs for the Project", isCompleted: true },
+    { id: 2, text: "Report Daily Progress", isCompleted: true },
+    { id: 3, text: "Checking for New Features", isCompleted: false },
+  ];
+
   return (
     <>
       <Header />
@@ -15,11 +23,10 @@ export function App() {
         <section className="w-full md:w-2/3 bg-white shadow-lg overflow-y-auto">
           <h2 className="text-2xl font-bold">
             <span className="ml-4">Tasks</span>
-            <TaskList />
+            <TaskList taskItems={taskItems} />
           </h2>
         </section>
       </main>
-      
     </>
   );
 }
